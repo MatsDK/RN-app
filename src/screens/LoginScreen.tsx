cf14959678b8cb6d3c702911ebfe7ca1dfd12ecd
MatsDK <67562518+MatsDK@users.noreply.github.com>
2022-03-27 15:44:11 +0200

    style login/signup screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import { Button, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { authScreenNavigationType } from '../contexts/userContext';
 import { auth } from "../firebase";
+import { MaterialCommunityIcons, Ionicons, Feather } from "@expo/vector-icons"
 
 interface LoginScreenProps {
 
@@ -29,26 +30,33 @@ export const LoginScreen: React.FC<LoginScreenProps> = () => {
 			style={styles.container}
 			behavior={"padding"}
 		>
-			<View>
-				<TextInput
-					style={styles.input}
-					placeholder="Email"
-					value={email}
-					onChange={text => setEmail(text.nativeEvent.text)}
-				/>
-				<TextInput
-					style={styles.input}
-					placeholder="Password"
-					secureTextEntry
-					value={password}
-					onChange={text => setPassword(text.nativeEvent.text)}
-				/>
-			</View>
-			<View style={styles.buttonContainer}>
+			<Text style={styles.title}>Login</Text>
+			<View style={styles.inputsContainer}>
+				<View style={styles.inputWrapper}>
+					<MaterialCommunityIcons style={styles.icon} name="email-outline" size={26} color="#333" />
+					<TextInput
+						style={styles.input}
+						placeholder="Email"
+						value={email}
+						onChange={text => setEmail(text.nativeEvent.text)}
+					/>
+				</View>
+				<View style={styles.inputWrapper}>
+					<MaterialCommunityIcons style={styles.icon} name="form-textbox-password" size={26} color="#333" />
+					<TextInput
+						style={styles.input}
+						placeholder="Password"
+						secureTextEntry
+						value={password}
+						onChange={text => setPassword(text.nativeEvent.text)}
+					/>
+				</View>
 				<TouchableOpacity onPress={login}>
 					<Text style={styles.button}>Login</Text>
 				</TouchableOpacity>
-				<Button onPress={() => navigation.navigate("SignUp")} title={"Sign Up"} />
+				<TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
+					<Text style={styles.createAccountButton}>Create an account</Text>
+				</TouchableOpacity>
 			</View>
 		</KeyboardAvoidingView>
 	);
@@ -56,29 +64,56 @@ export const LoginScreen: React.FC<LoginScreenProps> = () => {
 
 const styles = StyleSheet.create({
 	container: {
-		paddingTop: 150,
-		maxWidth: "60%",
-		marginLeft: 50
+		paddingTop: 100,
+		display: "flex",
+		justifyContent: "center",
+		padding: 40
+	},
+	inputsContainer: {
+		display: "flex",
+		flexDirection: "column",
+	},
+	title: {
+		fontSize: 35,
+		fontWeight: "500",
+		marginBottom: 30
 	},
 	buttonContainer: {
 		display: "flex",
 		alignItems: "flex-start"
 	},
 	button: {
+		marginTop: 15,
 		backgroundColor: "#000",
-		padding: 5,
+		paddingVertical: 5,
+		textAlign: "center",
 		color: "#fff",
 		borderRadius: 6,
 		overflow: "hidden",
-		fontSize: 18
+		fontSize: 20,
+	},
+	inputWrapper: {
+		borderWidth: 1,
+		borderColor: "#ddd",
+		borderRadius: 8,
+		width: "100%",
+		marginVertical: 5,
+		paddingVertical: 6,
+		paddingHorizontal: 10,
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center"
 	},
 	input: {
-		borderWidth: 1,
-		borderColor: "#ccc",
-		borderRadius: 5,
-		width: "100%",
-		fontSize: 18,
-		margin: 4,
-		padding: 2
+		fontSize: 20,
+		flex: 1
+	},
+	icon: {
+		marginRight: 10
+	},
+	createAccountButton: {
+		marginTop: 15,
+		textAlign: "center",
+		fontSize: 16
 	}
 })
--- src/screens/SignupScreen.tsx
+++ src/screens/SignupScreen.tsx
@@ -1,15 +1,23 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import React, { useState } from 'react'
-import { View, Text, KeyboardAvoidingView, TextInput, TouchableOpacity, StyleSheet } from "react-native"
+import { View, Text, KeyboardAvoidingView, TextInput, TouchableOpacity, StyleSheet, Button } from "react-native"
 import { auth } from '../firebase'
+import { MaterialCommunityIcons, Ionicons, Feather } from "@expo/vector-icons"
+import { useNavigation } from '@react-navigation/core'
+import { StackNavigationProp } from '@react-navigation/stack'
+import { AuthStackParamList } from '../../App'
 
 interface SignupScreenProps {
 
 }
 
+export type authScreenNavigationType = StackNavigationProp<AuthStackParamList, "SignUp">
+
 export const SignupScreen: React.FC<SignupScreenProps> = () => {
+	const navigation = useNavigation<authScreenNavigationType>()
 	const [email, setEmail] = useState("")
 	const [password, setPassword] = useState("")
+	const [username, setUsername] = useState("")
 
 	const signUp = async () => {
 		try {
@@ -21,58 +29,110 @@ export const SignupScreen: React.FC<SignupScreenProps> = () => {
 	}
 
 	return (
-		<KeyboardAvoidingView>
-
-			<View>
-				<TextInput
-					style={styles.input}
-					placeholder="Email"
-					value={email}
-					onChange={text => setEmail(text.nativeEvent.text)}
-				/>
-				<TextInput
-					style={styles.input}
-					placeholder="Password"
-					secureTextEntry
-					value={password}
-					onChange={text => setPassword(text.nativeEvent.text)}
-				/>
-			</View>
-			<View style={styles.buttonContainer}>
+		<KeyboardAvoidingView style={styles.container}>
+			<TouchableOpacity onPress={() => navigation.navigate("Login")} style={styles.loginButton}>
+				<MaterialCommunityIcons style={styles.icon} name="arrow-left" size={26} color="#333" />
+				<Text style={styles.loginButtonText}>
+					Login
+				</Text>
+			</TouchableOpacity>
+			<Text style={styles.title}>Sign Up</Text>
+			<View style={styles.inputsContainer}>
+				<View style={styles.inputWrapper}>
+					<MaterialCommunityIcons style={styles.icon} name="email-outline" size={26} color="#333" />
+					<TextInput
+						style={styles.input}
+						placeholder="Email"
+						value={email}
+						onChange={text => setEmail(text.nativeEvent.text)}
+					/>
+				</View>
+				<View style={styles.inputWrapper}>
+					<Feather style={styles.icon} name="user" size={26} color="#333" />
+					<TextInput
+						style={styles.input}
+						placeholder="Username"
+						value={username}
+						onChange={text => setUsername(text.nativeEvent.text)}
+					/>
+				</View>
+				<View style={styles.inputWrapper}>
+					<MaterialCommunityIcons style={styles.icon} name="form-textbox-password" size={26} color="#333" />
+					<TextInput
+						style={styles.input}
+						placeholder="Password"
+						secureTextEntry
+						value={password}
+						onChange={text => setPassword(text.nativeEvent.text)}
+					/>
+				</View>
 				<TouchableOpacity onPress={signUp}>
-					<Text style={styles.button}>Login</Text>
+					<Text style={styles.button}>Sign Up</Text>
 				</TouchableOpacity>
 			</View>
-
 		</KeyboardAvoidingView>
-
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-		paddingTop: 150,
-		maxWidth: "60%",
-		marginLeft: 50
+		paddingTop: 100,
+		display: "flex",
+		justifyContent: "center",
+		padding: 40
+	},
+	title: {
+		fontSize: 35,
+		fontWeight: "500",
+		marginBottom: 30
+	},
+	inputsContainer: {
+		display: "flex",
+		flexDirection: "column",
 	},
 	buttonContainer: {
 		display: "flex",
 		alignItems: "flex-start"
 	},
 	button: {
+		marginTop: 15,
 		backgroundColor: "#000",
-		padding: 5,
+		paddingVertical: 5,
+		textAlign: "center",
+		// paddingHorizontal: 15,
 		color: "#fff",
 		borderRadius: 6,
-		overflow: "hidden"
+		overflow: "hidden",
+		fontSize: 20,
+	},
+	loginButton: {
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+		// marginBottom: 10,
+		paddingVertical: 10
+	},
+	loginButtonText: {
+		fontSize: 18,
+		padding: 4,
+	},
+	inputWrapper: {
+		borderWidth: 1,
+		borderColor: "#ddd",
+		borderRadius: 8,
+		width: "100%",
+		marginVertical: 5,
+		paddingVertical: 6,
+		paddingHorizontal: 10,
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center"
 	},
 	input: {
-		borderWidth: 1,
-		borderColor: "#ccc",
-		borderRadius: 5,
-		width: "100%",
-		fontSize: 18,
-		margin: 4,
-		padding: 2
+		fontSize: 20,
+		flex: 1
+	},
+	icon: {
+		marginRight: 10
 	}
 })
